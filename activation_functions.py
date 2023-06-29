5a65a3ad89a4f9524ed3d9db4b4535f37670f83e
Michael Norman <normanm5@cardiff.ac.uk>
2023-06-29 06:34:39 -0700

    Added ipython activation function

--- activation_functions.py
+++ activation_functions.py
@@ -2,97 +2,203 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Define activation functions
-def linear(x):
-    return x
+from Typing import Tuple
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def create_plot(
+    x : np.ndarray, 
+    y, 
+    label : str, 
+    ax : matplotlib.axes._subplots.AxesSubplot, 
+    xlabel : str, 
+    ylabel : str, 
+    xlim : Tuple, 
+    ylim : Tuple, 
+    color : str):
+    """Creates a 2D plot.
+    
+    Parameters
+    ----------
+    x : np.ndarray
+        X values.
+    y : function
+        The function to apply to the x values.
+    label : str
+        Label for the plot.
+    ax : matplotlib.axes._subplots.AxesSubplot
+        The subplot object.
+    xlabel : str
+        X label.
+    ylabel : str
+        Y label.
+    xlim : Tuple
+        X axis limit.
+    ylim : Tuple
+        Y axis limit.
+    color : str
+        Color for the plot.
+    """
+    ax.plot(x, y(x), color=color, alpha=0.5, linewidth=3, label = label)
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    ax.legend()
 
-def relu(x):
-    return np.maximum(0, x)
+def create_3d_plot(x, y, z, label, ax, xlabel, ylabel, zlabel, color):
+    """Creates a 3D plot.
+    
+    Parameters
+    ----------
+    x : np.ndarray
+        X values.
+    y : np.ndarray
+        Y values.
+    z : np.ndarray
+        Z values calculated as per function.
+    label : str
+        Label for the plot.
+    ax : matplotlib.axes._subplots.Axes3DSubplot
+        The subplot object.
+    xlabel : str
+        X label.
+    ylabel : str
+        Y label.
+    zlabel : str
+        Z label.
+    color : str
+        Color for the plot.
+    """
+    surf = ax.plot_surface(x, y, z, color=color, alpha=0.5, label=label)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.invert_yaxis()
+    ax.set_zlabel(zlabel)
+    ax.view_init(elev=25, azim=-60)  # Set the viewing angle
+    surf._facecolors2d = surf._facecolor3d
+    surf._edgecolors2d = surf._edgecolor3d
+    ax.legend()
 
-def leaky_relu(x, alpha=0.01):
-    return np.where(x >= 0, x, alpha * x)
+def main():
+    # Define activation functions
+    def linear(x):
+        return x
 
-def elu(x, alpha=1):
-    return np.where(x >= 0, x, alpha * (np.exp(x) - 1))
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum()
+    def relu(x):
+        return np.maximum(0, x)
 
+    def leaky_relu(x, alpha=0.01):
+        return np.where(x >= 0, x, alpha * x)
 
+    def elu(x, alpha=1):
+        return np.where(x >= 0, x, alpha * (np.exp(x) - 1))
 
-x = np.linspace(-6, 6, 100)
-sigmoid_x = np.linspace(-6, 6, 100)
+    def softmax(x):
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
 
-fig = plt.figure(figsize=(12, 10))
+    x = np.linspace(-6, 6, 100)
+    fig = plt.figure(figsize=(12, 10))
 
-# Linear plot
-ax1 = fig.add_subplot(2, 2, 1)
-ax1.plot(x, linear(x), color="navy", alpha=0.5, linewidth=3, label = r"Linear, $\alpha = 1.0$")
-ax1.set_xlim([-6, 6])
-ax1.set_ylim([-6, 6])
-ax1.set_xlabel('z')
-ax1.set_ylabel('linear(z)')
-ax1.spines['right'].set_visible(False)
-ax1.spines['top'].set_visible(False)
-ax1.legend()
+    # Create each subplot
+    create_plot(
+        x, 
+        linear, 
+        r"Linear, $\alpha = 1.0$", 
+        fig.add_subplot(2, 2, 1), 
+        'z', 'linear(z)', 
+        [-6, 6], 
+        [-6, 6], 
+        "navy"
+    )
+    
+    create_plot(
+        x, 
+        sigmoid, 
+        "Sigmoid", 
+        fig.add_subplot(2, 2, 2), 
+        'z', 
+        '$\sigma$(z)', 
+        [-6, 6], 
+        [0, 1], 
+        "orange"
+    )
 
-# Sigmoid plot
-ax2 = fig.add_subplot(2, 2, 2)
-ax2.plot(sigmoid_x, sigmoid(sigmoid_x), color="orange", alpha=0.5, linewidth=3, label = "Sigmoid")
-ax2.set_xlim([-6, 6])
-ax2.set_ylim([0, 1])
-ax2.set_xlabel('z')
-ax2.set_ylabel('$\sigma$(z)')
-ax2.spines['right'].set_visible(False)
-ax2.spines['top'].set_visible(False)
-ax2.legend()
+    ax3 = fig.add_subplot(2, 2, 3)
+    create_plot(
+        x, 
+        relu, 
+        "ReLU", 
+        ax3, 
+        'z', 
+        'f(z)', 
+        [-6, 6], 
+        [-2, 6], 
+        "firebrick"
+    )
+    create_plot(
+        x, 
+        lambda x: leaky_relu(x, alpha=0.1), 
+        r"LeakyReLU, $\alpha = 0.1$", 
+        ax3, 
+        'z', 
+        'f(z)', 
+        [-6, 6], 
+        [-2, 6], 
+        "blue"
+    )
+    create_plot(
+        x, 
+        elu, 
+        r"ELU, $\alpha = 1.0$", 
+        ax3, 'z', 
+        'f(z)', 
+        [-6, 6], 
+        [-2, 6], 
+        "green"
+    )
 
+    _x = np.linspace(-6, 6, 100)
+    _y = np.linspace(-6, 6, 100)[::-1]
+    _xv, _yv = np.meshgrid(_x, _y)
 
-# ReLU plot
-ax3 = fig.add_subplot(2, 2, 3)
-ax3.plot(x, relu(x), color="firebrick", alpha=0.5, linewidth=3, label="ReLU")
-ax3.plot(x, leaky_relu(x, alpha = 0.1), color="blue", alpha=0.5, linewidth=3, label=r"LeakyReLU, $\alpha = 0.1$ ")
-ax3.plot(x, elu(x), color="green", alpha=0.5, linewidth=3, label=r"ELU, $\alpha = 1.0$ ")
-ax3.set_xlim([-6, 6])
-ax3.set_ylim([-2, 6])
-ax3.set_xlabel('z')
-ax3.set_ylabel('f(z)')
-ax3.spines['right'].set_visible(False)
-ax3.spines['top'].set_visible(False)
-ax3.legend()
+    # Compute softmax for each point on the grid
+    _z1 = np.array([softmax(np.array([x,y]))[0] for x, y in \
+                    zip(np.ravel(_xv), np.ravel(_yv))])
+    _z2 = np.array([softmax(np.array([x,y]))[1] for x, y in \
+                    zip(np.ravel(_xv), np.ravel(_yv))])
+    _z1 = _z1.reshape(_xv.shape)
+    _z2 = _z2.reshape(_yv.shape)
 
-# Softmax example
-_x = np.linspace(-6, 6, 100)
-_y = np.linspace(-6, 6, 100)[::-1]
-_xv, _yv = np.meshgrid(_x, _y)
+    ax4 = fig.add_subplot(2, 2, 4, projection='3d')
+    create_3d_plot(
+        _xv, 
+        _yv, 
+        _z1, 
+        r"$ softmax(\vec{z})_1 $", 
+        ax4, 
+        '$z_{1}$', 
+        '$z_{2}$', 
+        r'$ softmax(\vec{z})_i $', 
+        'b'
+    )
+    create_3d_plot(
+        _xv, 
+        _yv, 
+        _z2, 
+        r"$ softmax(\vec{z})_2 $", 
+        ax4, '$z_{1}$', 
+        '$z_{2}$', 
+        r'$ softmax(\vec{z})_i $', 
+        'r'
+    )
 
-# Compute softmax for each point on the grid
-_z1 = np.array([softmax(np.array([x,y]))[0] for x, y in zip(np.ravel(_xv), np.ravel(_yv))])
-_z2 = np.array([softmax(np.array([x,y]))[1] for x, y in zip(np.ravel(_xv), np.ravel(_yv))])
-_z1 = _z1.reshape(_xv.shape)
-_z2 = _z2.reshape(_yv.shape)
+    # Show the resulting plot
+    plt.savefig("activation_functions.png")
 
-ax4 = fig.add_subplot(2, 2, 4, projection='3d')
-surf1 = ax4.plot_surface(_xv, _yv, _z1, color='b', alpha=0.5, label=r"$ softmax(\vec{z})_1 $")
-surf2 = ax4.plot_surface(_xv, _yv, _z2, color='r', alpha=0.5, label=r"$ softmax(\vec{z})_2 $")
-ax4.set_xlabel('$z_{1}$')
-ax4.set_ylabel('$z_{2}$')
-ax4.invert_yaxis()
-ax4.set_zlabel(r'$ softmax(\vec{z})_i $')
-ax4.view_init(elev=25, azim=-60)  # Set the viewing angle
-
-surf1._facecolors2d = surf1._facecolor3d
-surf1._edgecolors2d = surf1._edgecolor3d
-
-surf2._facecolors2d = surf2._facecolor3d
-surf2._edgecolors2d = surf2._edgecolor3d
-
-ax4.legend()
-
-
-# Show the resulting plot
-plt.savefig("activation_functions.png")
+if __name__ == "__main__":
+    main()
