832e30e57c1a13e5edc235fcde3623f93f2968ce
Michael Norman <normanm5@cardiff.ac.uk>
2023-11-21 09:27:53 -0800

    Added hanford changes, confused

--- models/chapter_04/perceptron_128_128_safe/keras_metadata.pb
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a439f11214e47490495507ec78a8b7323b8ce025d03ba7bf5b96bf8cfdcf8299
-size 12857
--- plot_training.py
+++ plot_training.py
@@ -3,6 +3,16 @@ from pathlib import Path
 from bokeh.plotting import figure, output_file, save
 from bokeh.layouts import gridplot
 from bokeh.palettes import Category20  # A palette with 20 distinct colors
+from bokeh.embed import components, file_html
+from bokeh.io import export_png, output_file, save
+from bokeh.layouts import column, gridplot
+from bokeh.models import (ColumnDataSource, CustomJS, Dropdown, HoverTool, 
+                          Legend, LogAxis, LogTicker, Range1d, Slider, Select,
+                         Div)
+from bokeh.plotting import figure, show
+from bokeh.resources import INLINE, Resources
+from bokeh.palettes import Bright
+
 
 import numpy as np
 import gravyflow as gf
@@ -19,6 +29,18 @@ def transform_string(s):
 
     return ', '.join(map(str, parts))
 
+def snake_case_to_capitalised_first_with_spaces(text):
+    """
+    Convert a string from snake_case to Capitalised First With Spaces format.
+    """
+
+    text.replace('val', 'validate') if 'val' in text else text
+    # Split the string at underscores
+    words = text.split('_')
+    
+    # Capitalise the first letter of each word and join them with spaces
+    return ' '.join(word.capitalize() for word in words)
+
 def plot_metrics(metrics_dict):
     if len(metrics_dict) > 20:
         raise ValueError("Number of series exceeds the number of available colors in the palette")
@@ -32,20 +54,65 @@ def plot_metrics(metrics_dict):
         # Use specified width for each plot, or default if not enough widths are specified
         width = plot_widths[i] if i < len(plot_widths) else plot_widths[-1]
 
-        p = figure(title=key, x_axis_label='Epochs', y_axis_label=key, width=width)
+        y_axis_label = snake_case_to_capitalised_first_with_spaces(key)
+        if "accuracy" in key:
+            y_axis_label += " (Per Cent)"
         
+        p = figure(x_axis_label='Epochs', y_axis_label=y_axis_label, width=width)
+        
+        if "accuracy" in key:
+            p.y_range.start = 45
+            p.y_range.end = 100
+        else: 
+            p.y_range.start = 0.4
+            p.y_range.end = 1.0
+
+        # Increase font sizes
+        p.axis.axis_label_text_font_size = "14pt"  # Increase axis label font size
+        p.axis.major_label_text_font_size = "12pt"  # Increase tick label font size
+
+        # If you have titles
+        p.title.text_font_size = '16pt'
+
+        all_sources = {}
+
         # Plot each series in the list
         for idx, (name, metrics) in enumerate(metrics_dict.items()):
             if key in metrics:
                 color = Category20[20][idx % 20]
-                p.line(
-                    np.arange(len(metrics[key])), 
-                    metrics[key], 
-                    legend_label=name, 
+
+                y = metrics[key]
+                if "accuracy" in key:
+                    y = [100*i for i in y]
+
+                source = ColumnDataSource(
+                        data={"x": np.arange(len(metrics[key])), "y" : y, "name" : [name] * len(metrics[key])}
+                )
+                all_sources[name] = source
+                line = p.line(
+                    x='x', 
+                    y='y', 
+                    source=source, 
                     line_color=color,
+                    legend_label=name
                 )
 
+         # If you have a legend
+        p.legend.location = "top_right"
+        p.legend.label_text_font_size = "12pt"
+        p.legend.click_policy = "hide"
+        
+        hover = HoverTool()
+        if "accuracy" in key:
+            hover.tooltips = [("Name", "@name"), ("Epoch", "@x"), ("Accuracy", "@y")]
+        else:
+            hover.tooltips = [("Name", "@name"), ("Epoch", "@x"), ("Loss", "@y")]
+
+        p.add_tools(hover)
+        
         figures.append(p)
+
+
 
     # Layout the plots in a grid
     grid = gridplot(figures, ncols=2)
