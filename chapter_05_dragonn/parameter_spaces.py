684bdf4abdfb25c588299411cee6c9862e6cb623
Michael Norman <normanm5@cardiff.ac.uk>
2023-12-28 15:30:39 -0800

    Updated CrossWave and Dragonn

--- chapter_05_dragonn/parameter_spaces.py
+++ chapter_05_dragonn/parameter_spaces.py
@@ -3,27 +3,13 @@ from bokeh.plotting import figure, show, output_file, output_notebook
 from bokeh.models import ColorBar, LinearColorMapper
 from bokeh.palettes import Sunset11
 
+from skopt import gp_minimize
+from skopt.space import Real
+
 def random_peak(x, y):
     a = np.random.uniform(-4.0,4.9, size = 6)
     
     return a[0] * np.exp(-((x / a[1] - a[2])**2 + (y / a[3] - a[4])**2 / (2*a[5]**2)))
-
-def objective_function(x, y):
-    """
-    Multimodal synthetic objective function.
-    """
-    # Three different Gaussian functions with different means and standard deviations
-    value = 2.0 * np.exp(-((x - 2)**2 + (y - 2)**2) / (2 * 0.8**2))
-    value += 1.5 * np.exp(-((x + 2.5)**2 + (y + 2.5)**2) / (2 * 1.5**2))
-    value += 3.0 * np.exp(-((x - 1.5)**2 + (y + 3)**2) / (2 * 0.5**2))
-    
-    # Add a negative elliptical shape
-    value -= 1.0 * np.exp(-((x / 2.0)**2 + (y / 1.5)**2))
-    value += 2.2 * np.exp(-((0.1 * x / 0.1)**2 + (0.25 * y / 4.0)**2))
-
-    value -= 1.5 * np.exp(-((x / 1.8)**2 + (4 * y / 1.5)**2))
-
-    return value
 
 def objective_function(x, y):
     
@@ -31,34 +17,9 @@ def objective_function(x, y):
     for _ in range(100):
         value += random_peak(x, y)
         
-    print(value)
-    
     return value
 
-def plot_objective_function(X, Y, Z, levels=15, filename="output.html"):
-    # Reverse the Viridis palette for better visual
-
-    """
-    color_mapper = LinearColorMapper(palette=Viridis256, low=Z.min(), high=Z.max())
-
-    # Initialize a figure
-    p = figure(title="Objective Function", toolbar_location="right",
-               x_range=(X.min(), X.max()), y_range=(Y.min(), Y.max()))
-
-    # Add contour plot to the figure
-    contour = Contour(x=X, y=Y, z=Z, levels=levels, line_color=None, fill_color={'field': 'z', 'transform': color_mapper})
-    p.add_tools(contour)
-
-    # Add color bar
-    color_bar = ColorBar(color_mapper=color_mapper, location=(0, 0))
-    p.add_layout(color_bar, 'right')
-    """
-
-    # Save the bokeh figure to an HTML file
-    output_file(filename)
-    show(p)
-    
-def plot_objective_function(X, Y, Z, filename="output.html"):
+def plot_objective_function(X, Y, Z, search_points = None, filename="output.html"):
     # Creating the figure object
     p = figure(width=600, height=500, toolbar_location=None, x_range=(X.min(), X.max()), y_range=(Y.min(), Y.max()),
                title="Contour plot of objective function")
@@ -77,11 +38,45 @@ def plot_objective_function(X, Y, Z, filename="output.html"):
     # Adding color bar to the figure
     p.add_layout(colorbar, 'right')
 
+     # Plot search points if provided
+    if search_points is not None:
+        p.circle(search_points[:, 0], search_points[:, 1], size=10, color="red", alpha=0.6)
+    
     # Save the plot to an html file
     output_file(filename)
     show(p)
 
+def grid_search(x_range, y_range, steps=10):
+    x_points = np.linspace(x_range[0], x_range[1], steps)
+    y_points = np.linspace(y_range[0], y_range[1], steps)
+    X, Y = np.meshgrid(x_points, y_points)
+    points = np.vstack([X.ravel(), Y.ravel()]).T
+    return points
 
+def random_search(x_range, y_range, num_samples=100):
+    x_points = np.random.uniform(x_range[0], x_range[1], num_samples)
+    y_points = np.random.uniform(y_range[0], y_range[1], num_samples)
+    points = np.vstack([x_points, y_points]).T
+    return points
+
+def bayesian_optimization(objective, x_range, y_range, n_calls=100):
+    def wrapped_objective(xy):
+        # Pass the tuple xy directly to the objective function
+        return -objective(xy)  # Negate for minimization
+
+    res = gp_minimize(wrapped_objective, [Real(x_range[0], x_range[1]), Real(y_range[0], y_range[1])],
+                      n_calls=n_calls, random_state=5, n_initial_points = 100)
+
+    return np.array(res.x_iters)
+
+def modified_objective_function(xy):
+    x, y = xy
+    # Since x and y are single values, calculate the objective function value for this point
+    value = objective_function(np.array(x), np.array(y))
+    # Make sure to return a scalar; here we return the first element of the array
+    return value.item()
+
+np.random.seed(100)
 # Define the grid for visualization purposes
 x = np.linspace(-6, 6, 1000)
 y = np.linspace(-6, 6, 1000)
@@ -90,3 +85,12 @@ Z = objective_function(X, Y)
 
 # Call the function to plot the objective function
 plot_objective_function(X, Y, Z)
+
+grid_points = grid_search([-6, 6], [-6, 6], steps=10)
+plot_objective_function(X, Y, Z, search_points=grid_points, filename="grid_search_output.html")
+
+grid_points = random_search([-6, 6], [-6, 6], num_samples=100)
+plot_objective_function(X, Y, Z, search_points=grid_points, filename="random_search_output.html")
+
+grid_points = bayesian_optimization(modified_objective_function, [-6, 6], [-6, 6], n_calls=200)
+plot_objective_function(X, Y, Z, search_points=grid_points, filename="bayesian_search_output.html")
